db0c4f93f1e9cc11a6c57c31141e198c382ce663
William Hou <williamhou38@yahoo.com>
2023-11-22 17:31:04 -0500

    first commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Test recipe 1', 'This is simply a test', 'https://s3-media0.fl.yelpcdn.com/bphoto/yzw4zAF9utVQtn6jIuRa8w/o.jpg'),
+    new Recipe('Test recipe 2', 'This is simply a test', 'https://s3-media0.fl.yelpcdn.com/bphoto/ABgd0Jy2plKdofnHksXr-g/o.jpg'),
+    new Recipe('Test recipe 2', 'This is simply a test', 'https://s3-media0.fl.yelpcdn.com/bphoto/z7HsOPZvIC2iQry2jf2CJg/o.jpg'),
+    new Recipe('Test recipe 3', 'This is simply a test', 'https://s3-media0.fl.yelpcdn.com/bphoto/vIG3h1tfFimspbvhlGeB9w/o.jpg')
+  ];
 }
